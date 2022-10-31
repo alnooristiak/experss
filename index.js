1b9ccf04cbcface8c296851b8848ca7c8eae9b32
Al Noor Istiak Mahmud <istiakmahmud1010@gmail.com>
2022-10-31 19:58:36 +0600

    adding html tag and json data loding

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const PORT = 5000;
+
 
 // Home page for app 
 app.get("/", (req, res) => {
@@ -11,8 +13,20 @@ app.get("/about", (req, res) => {
     res.send("hellow world this is about page");
 });
 
+// adding html element 
+app.get("/html", (req, res) => {
+    res.write("<h2>This is a h2 tag in html</h2>");
+});
+
+// json fake data loding 
+app.get("/jsondata", (req, res) => {
+    res.send({
+        id: 1,
+        name: "istiak",
+    });
+});
 
 // listening port on webpage http url req 
-app.listen(5000, () => {
-    console.log("listining port are 5000");
+app.listen(PORT, () => {
+    console.log(`listining port are ${5000}`);
 });
