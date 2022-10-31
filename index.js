b2d62228204f63d0d27177511c70a657d1d0eba2
Al Noor Istiak Mahmud <istiakmahmud1010@gmail.com>
2022-10-31 19:19:56 +0600

    adding home page route

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const app = express();
 
+// Home page for app 
 app.get("/", (req, res) => {
     res.send("hellow world");
 });
 
+// about page 
+app.get("/about", (req, res) => {
+    res.send("hellow world this is about page");
+});
+
+
+// listening port on webpage http url req 
 app.listen(5000, () => {
     console.log("listining port are 5000");
 });
